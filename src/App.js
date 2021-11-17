1d1967197e5e28247d4f63357329686064d8c88f
palvi tiwari <palvitiwari913@gmail.com>
2021-11-17 15:46:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import "./components/style.css";
 import "./components/common.css";
-
 import ProfileJoyrideProvider from "./components//useContext/SidebarProvider";
-
 import Router from "./components/common/Router";
 
 function App() {
