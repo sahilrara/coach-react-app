1acbe64aa36c4d20790f4adc2a2dee9711d69ebc
palvi tiwari <palvitiwari913@gmail.com>
2021-12-02 14:23:06 +0530

    contact ui

---
+++ src/redux/apis/Contact.jsx
@@ -0,0 +1,37 @@
+import { axiosRequest } from "../helper";
+
+/**
+ * get all user list Api
+ * @param {Number} page
+ * @returns
+ */
+export const GetAllUserListApi = async (page) => {
+  return await axiosRequest("GET", `/admin/contacts?page=${page}&limit=10`);
+};
+
+/**
+ * User details api
+ * @param {Object} data
+ * @returns
+ */
+export const getUserDetailsApi = async (value) => {
+  return await axiosRequest("GET", `/user/detail/${value}`);
+};
+
+/**
+ * Update user details api
+ * @param {Object} data
+ * @returns
+ */
+export const updateUserDetailsApi = async (userId, data) => {
+  return await axiosRequest("PUT", `/update/user/${userId}`, data);
+};
+
+/**
+ * Delete user details api
+ * @param {Object} data
+ * @returns
+ */
+export const DeleteUserApi = async (userId) => {
+  return await axiosRequest("DELETE", `/admin/user/delete/${userId}`);
+};
